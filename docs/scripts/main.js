2472179d92c6326cf8b8291e66f0ef6db7a5ab29
wolfram74 <phaugen@gmail.com>
2017-07-08 09:25:57 -0500

    set up basic templates to load

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,1 +1,4 @@
 console.log('main loaded')
+
+const inhouse = new Vue({el:'#jsVueInHouseLinks'})
+const external = new Vue({el:'#jsVueExternalLinks'})
--- docs/scripts/templates.js
+++ docs/scripts/templates.js
@@ -1,1 +1,19 @@
 console.log('templates loaded')
+Vue.component('inhouse-links',{
+  template:`
+  <div class='link-container'>
+    <h3>ollo</h3>
+  </div>
+  `
+})
+
+
+Vue.component('external-links',{
+  template:`
+  <div class='link-container'>
+    <h3>ollo</h3>
+  </div>
+  `
+})
+
+
